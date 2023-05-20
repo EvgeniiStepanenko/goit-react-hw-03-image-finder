d096e41343190cae1111177f9c383b1897c0492a
EvgeniiStepanenko <115147549+EvgeniiStepanenko@users.noreply.github.com>
2023-05-20 11:35:21 +0300

    imgFinder

---
+++ src/api/getSearch.jsx
@@ -0,0 +1,16 @@
+const API_KEY = '35257003-3888449e228f8cae772a00d8b';
+const BASE_URL = 'https://pixabay.com/api/';
+const PICS_ON_PAGE = 12;
+
+export const getSearch = (searchText, page) => {
+  const params = new URLSearchParams({
+    q: searchText,
+    page: page,
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: PICS_ON_PAGE,
+  });
+
+  return fetch(`${BASE_URL}?${params}`);
+};
